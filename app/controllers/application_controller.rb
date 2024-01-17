cedf3f66793e8d2d06cc6cf4dfad4d09d1225c16
tlotht <tlotht@163.com>
2024-01-17 11:26:20 +0800

    设置全局分页

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  def per_page
+    if params[:per_page].present?
+      a = params[:per_page].to_i
+    else
+      a = 20
+    end
+    return a 
+  end
+
+  def page
+    if params[:page].present?
+      a = params[:page].to_i
+    else
+      a = 1
+    end
+    return [1,a].max 
+  end
 end
