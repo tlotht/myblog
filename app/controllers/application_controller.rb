4494b093b9fb983bb6348ea3143699542931f237
tlotht <tlotht@163.com>
2024-02-19 11:40:49 +0800

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
-  # helper_method :current_user
+  helper_method :current_user
   helper_method :user_signed_in?
-  # before_action :authenticate_user
+  #before_action :authenticate_user
 
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id]) 
@@ -16,8 +16,16 @@ class ApplicationController < ActionController::Base
   #   redirect_to new_session_path, flash: {danger: 'You must be signed in'} if current_user.nil?
   # end
 
+  # def authenticate
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == "money" && password == "money"
+  #   end
+  # end
+
+
+
   # def redirect_if_authenticated
-  #   redirect_to root_path, flash: { info: 'You are already logged in.'} if user_signed_in?
+  #  redirect_to root_path, flash: { info: 'You are already logged in.'} if user_signed_in?
   # end
 
   def per_page
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
 
 # == Schema Information
